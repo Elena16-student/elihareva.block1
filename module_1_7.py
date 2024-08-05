5143297553b1ea3117daa2315f55f8773f070acd
Elena16-student <usovk@mail.ru>
2024-08-05 19:17:22 +0300

    Add files via upload

--- module_1_7.py
+++ module_1_7.py
@@ -1,27 +1,21 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-#print(type(grades))
-Student = list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
-print(type(Student))
-#middle = (sum(grades[0:]))/len(grades[0:]) #ошибка unsupported operand type(s) for +: 'int' and 'list'
+Student = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+Student_alph = sorted(Student)
+#print(Student_alph)
+A = [5, 3, 3, 5, 4]
+middle = sum(A)/len(A)
 #print(middle)
-n = sum(grades[0])
-m = len(grades[0])
-n_ = (n/m)
-a = sum(grades[1])
-b = len(grades[1])
-a_ = (a/b)
-c_ = sum(grades[2])
-c__ = len(grades[2])
-c = (c_/c__)
-d_ = sum(grades[3])
-d__ = len(grades[3])
-d = (d_/d__)
-r_ = sum(grades[4])
-r__ = len(grades[4])
-r = (r_/r__)
-#q = (sum(grades[0]), sum(grades[1]), sum(grades[2]), sum(grades[3]), sum(grades[4]))
-#z = (len(grades[0]), len(grades[1]), len(grades[2]), len(grades[3]), len(grades[4]))
-#p = str(q[:]) / str(z[:])
-#print(p)
-middle_mark = dict([(Student[0], n_,), (Student[1], a_), (Student[2], c), (Student[3], d),(Student[4], r),])
-print(middle_mark)
+middle_mark_A = sum(grades[0])/len(grades[0])
+middle_mark_B = sum(grades[1])/len(grades[1])
+middle_mark_J = sum(grades[2])/len(grades[2])
+middle_mark_K = sum(grades[3])/len(grades[3])
+middle_mark_S = sum(grades[4])/len(grades[4])
+middle_mark_1 = ((sum(grades[0])/len(grades[0])), (sum(grades[1])/len(grades[1])),
+                 (sum(grades[2])/len(grades[2])), (sum(grades[3])/len(grades[3])), (sum(grades[4])/len(grades[4])))
+mark = ((Student_alph[0], middle_mark_A), (Student_alph[1], middle_mark_B), (Student_alph[2],
+middle_mark_J), (Student_alph[3], middle_mark_K), (Student_alph[4], middle_mark_S))
+dict1 = dict(mark)
+print(dict1)
+#Dict2 = dict(zip(Student_alph, middle_mark_1)) # , более простое решение
+#print(Dict2)
+
