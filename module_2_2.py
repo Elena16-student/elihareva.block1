3b946e82f11973272bcf426519e184f6d8dbfb5b
Elena16-student <usovk@mail.ru>
2024-08-06 18:37:31 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,28 @@
+first = int(input('Введите число:'))
+second = int(input('Введите число:'))
+third = int(input('Введите число:'))
+
+if first == third == second:
+    print(3)
+elif first == second:
+    print(2)
+elif first == third:
+    print(2)
+elif first == third:
+    print(2)
+else:
+    print(0)
+first_ = int(input('Введите число:'))
+second_ = int(input('Введите число:'))
+third_ = int(input('Введите число:'))
+
+if first_ == third_ == second_:
+    print(3)
+elif first_ == second_:
+    print(2)
+elif first_ == third_:
+    print(2)
+elif first_ == third_:
+    print(2)
+else:
+    print(0)
