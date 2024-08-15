5a9ada8e47e8e179813b24e8a80b5e8e7a400afa
Elena16-student <usovk@mail.ru>
2024-08-15 18:58:22 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,24 +1,23 @@
+
 def send_email(massage, recipient, *, sender="university.help@gmail.com"):
-    while 1 > 0:
-        if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
-            if '@' not in recipient and sender:
-                print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-                break
-        else:
+
+        if '@' not in recipient or '@' not in sender \
+            or not recipient.endswith(('.com', '.ru', '.net')) or not sender.endswith(('.com', '.ru', '.net')):
+
             print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-            break
-        if sender == recipient:
-            print("Нельзя отправить письмо самому себе!")
-            break
-        elif sender == "university.help@gmail.com":
-            print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
-            break
+
+        elif sender == recipient:
+            print(f"Нельзя отправить письмо самому себе!")
+
         elif sender != "university.help@gmail.com":
             print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
-            break
+
+        else:
+            print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
 
 
-send_email('Это сообщение для проверки связи', 'vasyok1337gmail.com')
+
+
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'puple@gmail.com', sender='urban.teacher@mail.uk')
