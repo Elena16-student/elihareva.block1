f0534288e6935534feab98514e65161c6f5b701c
Elena16-student <usovk@mail.ru>
2024-09-26 08:29:10 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,56 @@
+
+import io
+from pprint import pprint
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+def custom_write(file_name, strings):
+    strings_positions = {}
+    file = open(file_name, 'a', encoding='utf-8')
+
+
+    for i in strings:
+
+        position = file.tell()
+        file.write(strings + '\n')
+        strings_positions[(i, position)] = strings
+    file.close()
+    return strings_positions
+
+
+
+
+def __main__():
+    result = custom_write('test.txt', info)
+    for elem in result.items():
+        print(elem)
+
+# from pprint import pprint
+# # Создаём функцию custom_write (file_name, strings)
+# # которая принимает аргументы file_name - название файла для записи,
+# # strings - список строк для записи.
+# def custom_write(file_name, strings):
+#     strings_positions = {}
+#     with open(file_name, 'w', encoding='utf-8') as file:
+#         for i, string in enumerate(strings, start=1):
+# # Получение номера байта начала строки используем метод tell() перед записью
+#             position = file.tell()
+#             file.write(string + '\n')
+#             strings_positions[(i, position)] = string
+#     return strings_positions
+#
+#
+# info = [
+#     'Text for tell.',
+#     'Используйте кодировку utf-8.',
+#     'Because there are 2 languages!',
+#     'Спасибо!'
+#     ]
+# # Вывод на консоль:
+# result = custom_write('test.txt', info)
+# for elem in result.items():
+#   print(elem)
