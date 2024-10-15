e4dfb8aa5b274afc3d3b617f12d2e551010f1c18
Elena16-student <usovk@mail.ru>
2024-10-15 21:28:52 +0300

    Add files via upload

--- module_9_7.py
+++ module_9_7.py
@@ -3,18 +3,19 @@ def is_prime(func):
         result = func(*args)
         #if result > 1: #Начинаем перебор с 2, т.к. единица вне конкуренции (не проста,  но и не составная)
         if all((result % i != 0) for i in range(2, int(result))):
-            return "Простое" #простое число делится на 1 и на самое себя
+             #return (f'{result} \nПростое') # как вариант 1
+             print("Простое") #  вариант попроще
+             return result
         else:
-            return 'Cоставное'
+            #return (f'{result} \nCoставное') # как вариант 1
+            print("Cоставное")
+            return result
     return wrapper
 
-
 @is_prime
-def sum_three(a, b, c):
-    result = a + b + c
-    print(result)
+def sum_three(*args):
+    result = sum(args)
     return result
-
 
 test_1 = sum_three(3, 7, 3)
 print(test_1)
